fe16567ee66f046e7407050e595f23b470ef5add
Lost Paul <70213368+LostPaul@users.noreply.github.com>
2025-01-09 22:50:34 +0100

    Fix collapse icon

--- src/FileExplorer.ts
+++ src/FileExplorer.ts
@@ -1,4 +1,4 @@
-import { MarkdownPostProcessorContext, parseYaml, TAbstractFile, TFolder, TFile, stringifyYaml, Notice, Menu, setIcon } from 'obsidian';
+import { MarkdownPostProcessorContext, parseYaml, TAbstractFile, TFolder, TFile, stringifyYaml, Notice, Menu, setIcon, addIcon } from 'obsidian';
 import { getFolderNote } from '../../functions/folderNoteFunctions';
 import { FolderOverviewSettings } from './modals/Settings';
 import { getExcludedFolder } from '../../ExcludeFolders/functions/folderFunctions';
@@ -198,7 +198,6 @@ export class FileExplorerOverview {
         }
         if (excludedFolder?.excludeFromFolderOverview) { return; }
 
-        const svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon right-triangle"><path d="M3 8L12 17L21 8"></path></svg>';
         if (yaml.includeTypes.includes('folder')) {
             folderElement = childrenElement.createDiv({
                 cls: 'tree-item nav-folder',
@@ -292,7 +291,8 @@ export class FileExplorerOverview {
         }
 
         if (collapseIcon) {
-            setIcon(collapseIcon, svg);
+            setIcon(collapseIcon, 'chevron-down');
+            collapseIcon.querySelector('path')?.setAttribute('d', 'M3 8L12 17L21 8');
             collapseIcon.onclick = () => {
                 this.handleCollapseClick(collapseIcon, plugin, yaml, pathBlacklist, sourceFolderPath, folderOverview, child);
             }
