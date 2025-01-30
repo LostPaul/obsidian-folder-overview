a1f5c18862a21ff27071242081d3b4e97141098f
Lost Paul <70213368+LostPaul@users.noreply.github.com>
2025-01-30 12:07:31 +0100

    Fix that folders still show in the list even though you removed them from the list

--- src/ListStyle.ts
+++ src/ListStyle.ts
@@ -7,7 +7,7 @@ import FolderNotesPlugin from '../../main';
 
 export function renderListOverview(plugin: FolderOverviewPlugin | FolderNotesPlugin, ctx: MarkdownPostProcessorContext, root: HTMLElement, yaml: overviewSettings, pathBlacklist: string[], folderOverview: FolderOverview) {
     const overviewList = folderOverview.listEl;
-        overviewList?.empty();
+    overviewList?.empty();
     let tFolder = plugin.app.vault.getAbstractFileByPath(yaml.folderPath);
     if (!tFolder && yaml.folderPath.trim() == '') {
         if (ctx.sourcePath.includes('/')) {
@@ -28,9 +28,13 @@ export function renderListOverview(plugin: FolderOverviewPlugin | FolderNotesPlu
     files = folderOverview.sortFiles(files.filter(f => f instanceof TFile));
     folders.forEach((file) => {
         if (file instanceof TFolder) {
-            const folderItem = addFolderList(plugin, ul, folderOverview.pathBlacklist, file, folderOverview);
-            if (!folderItem) { return; }
-            goThroughFolders(plugin, folderItem, file, folderOverview.yaml.depth, sourceFolderPath, ctx, folderOverview.yaml, folderOverview.pathBlacklist, folderOverview.yaml.includeTypes, folderOverview.yaml.disableFileTag, folderOverview);
+            if (yaml.includeTypes.includes('folder')) {
+                const folderItem = addFolderList(plugin, ul, folderOverview.pathBlacklist, file, folderOverview);
+                if (!folderItem) { return; }
+                goThroughFolders(plugin, folderItem, file, folderOverview.yaml.depth, sourceFolderPath, ctx, folderOverview.yaml, folderOverview.pathBlacklist, folderOverview.yaml.includeTypes, folderOverview.yaml.disableFileTag, folderOverview);
+            } else {
+                goThroughFolders(plugin, ul, file, folderOverview.yaml.depth, sourceFolderPath, ctx, folderOverview.yaml, folderOverview.pathBlacklist, folderOverview.yaml.includeTypes, folderOverview.yaml.disableFileTag, folderOverview);
+            }
         }
     });
 
@@ -110,15 +114,23 @@ async function goThroughFolders(plugin: FolderOverviewPlugin | FolderNotesPlugin
 
     folders.forEach((file) => {
         if (file instanceof TFolder) {
-            const folderItem = addFolderList(plugin, ul, pathBlacklist, file, folderOverview);
-            if (!folderItem) return;
-            goThroughFolders(plugin, folderItem, file, depth, sourceFolderPath, ctx, yaml, pathBlacklist, includeTypes, disableFileTag, folderOverview);
+            if (yaml.includeTypes.includes('folder')) {
+                const folderItem = addFolderList(plugin, ul, pathBlacklist, file, folderOverview);
+                if (!folderItem) { return; }
+                goThroughFolders(plugin, folderItem, file, depth, sourceFolderPath, ctx, yaml, pathBlacklist, includeTypes, disableFileTag, folderOverview);
+            } else {
+                goThroughFolders(plugin, list, file, depth, sourceFolderPath, ctx, yaml, pathBlacklist, includeTypes, disableFileTag, folderOverview);
+            }
         }
     });
 
     files.forEach((file) => {
         if (file instanceof TFile) {
-            addFileList(plugin, ul, pathBlacklist, file, includeTypes, disableFileTag, folderOverview);
+            if (yaml.includeTypes.includes('folder')) {
+                addFileList(plugin, ul, pathBlacklist, file, includeTypes, disableFileTag, folderOverview);
+            } else {
+                addFileList(plugin, list, pathBlacklist, file, includeTypes, disableFileTag, folderOverview);
+            }
         }
     });
 }
